0eda6f9da5a41c1917b8aceda3c4fc0ce9fd6efd
Blaine <rishi1998@gmail.com>
2020-07-21 22:09:26 +0530

    add build process

---
+++ build.rs
@@ -0,0 +1,11 @@
+#[cfg(windows)]
+extern crate winres;
+
+#[cfg(windows)]
+fn main() {
+  if cfg!(target_os = "windows") {
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("assets/Icon.ico");
+    res.compile().unwrap();
+  }
+}
